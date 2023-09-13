7f669b1e1ad44f090279ada6298910e7d001e2c1
= <=>
2023-09-13 15:59:18 -0600

    Hello, name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
-}
+fn main(){
+    let mut line = String::new();
+    println!("Enter your name :");
+    let _b1 = std::io::stdin().read_line(&mut line).unwrap();
+    println!("Hello , {}", line);
+ }
